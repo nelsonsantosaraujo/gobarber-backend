f66a086f69016aeea01dd0b83b144a78fdf63c77
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2020-11-05 22:10:44 -0300

    Iniciando o projeto

---
+++ src/database/migrations/1604624488063-CreateAppointment.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointment1604624488063 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
