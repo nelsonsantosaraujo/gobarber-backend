65fb26ef896ff8bad4edf644dfc9c5adac9cae35
Nelson Santos Araújo <slyckers@hotmail.com>
2020-12-18 11:45:02 -0300

    Criando o primeiro teste unitario

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
-// import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    console.log('teste');
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
   });
 
   // it('should not be able to create two appointments on the same time', () => {
