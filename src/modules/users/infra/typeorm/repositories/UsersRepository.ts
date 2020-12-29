f46ec064977de92ff0887b0b98aaab71ec5c2d31
Nelson Araújo <slyckers@hotmail.com>
2020-12-29 12:50:40 -0300

    correção: Adicionando metodo save no create User

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -46,6 +46,8 @@ class UsersRepository implements IUsersRepository {
   public async create(userData: ICreateUserDTO): Promise<User> {
     const user = this.ormRepository.create(userData);
 
+    await this.ormRepository.save(user);
+
     return user;
   }
 
